1a5a51acede5d8c55df5361053be0571070b5619
bitfurytech21-eng <bitfurytech21@gmail.com>
2026-07-24 11:55:54 +0100

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -2,14 +2,18 @@ import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
+
 class Config:
-    SECRET_KEY = os.getenv(
+    SECRET_KEY = os.environ.get(
         "SECRET_KEY",
         "bitfury-tech-investment-secret"
     )
 
-    SQLALCHEMY_DATABASE_URI = (
-        "sqlite:///" + os.path.join(BASE_DIR, "database", "bitfurydb.db")
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL",
+        f"sqlite:///{os.path.join(BASE_DIR, 'database', 'bitfury.db')}"
     )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    JSON_SORT_KEYS = False
