6d77fbde37a3fa63bc9efed3991c43e0ef7c827a
bitfurytech21-eng <bitfurytech21@gmail.com>
2026-07-24 17:35:12 +0100

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,46 +1,7 @@
 import "./App.css";
 
-import Hero from "./components/Hero";
-import About from "./components/About";
-import CompanyVideo from "./components/CompanyVideo";
-import WhyChooseUs from "./components/WhyChooseUs";
-import MarketOverview from "./components/MarketOverview";
-import Plans from "./components/Plans";
-import Payments from "./components/Payments";
-import BoardSection from "./components/board/BoardSection";
-import Footer from "./components/Footer";
-
 function App() {
-  return (
-    <div className="App">
-      {/* Hero */}
-      <Hero />
-
-      {/* About Us */}
-      <About />
-
-      {/* Company Introduction Video */}
-      <CompanyVideo />
-
-      {/* Why Choose Us */}
-      <WhyChooseUs />
-
-      {/* Market Overview */}
-      <MarketOverview />
-
-      {/* Investment Plans */}
-      <Plans />
-
-      {/* Payment Methods */}
-      <Payments />
-
-      {/* Board of Directors */}
-      <BoardSection />
-
-      {/* Footer */}
-      <Footer />
-    </div>
-  );
+  return <h1>Bitfury Tech Investment</h1>;
 }
 
 export default App;
