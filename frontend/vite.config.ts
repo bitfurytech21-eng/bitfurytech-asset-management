eb9266482f2b880687d02e89540f4cacb5cddcd1
bitfurytech21-eng <bitfurytech21@gmail.com>
2026-07-24 15:34:19 +0100

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
 
             proxy: {
                   '/api': {
-                          target: 'http://127.0.0.1:5000',
+                          target: 'https://bitfurytech-asset-management-5.onrender.com',
                                   changeOrigin: true,
                                           secure: false
                                                 }
