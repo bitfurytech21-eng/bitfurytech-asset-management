843a9f37ccf586ac7966a784b9a8d96e91e1a2a5
bitfurytech21-eng <bitfurytech21@gmail.com>
2026-07-24 08:11:50 +0100

    Create __init__.py

---
+++ backend/models/__init__.py
@@ -0,0 +1,7 @@
+from .user import User
+from .investment import Investment
+from .transaction import Transaction
+from .payment import PaymentMethod
+from .notification import Notification
+from .board import BoardMember
+from .faq import FAQ
